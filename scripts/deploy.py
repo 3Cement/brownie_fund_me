8e77b3cbf54618f5bf0ab80c29fa11cc321448c9
daniel <danielmilewski123@gmail.com>
2023-09-19 16:24:53 +0200

    ganache-local added to networks

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,26 +1,22 @@
 from brownie import FundMe, MockV3Aggregator, network, config
-from scripts.helpful_scripts import get_account
+from scripts.helpful_scripts import (
+    get_account,
+    deploy_mocks,
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+)
 from web3 import Web3
 
 
 def deploy_fund_me():
+    print("network.show_active", network.show_active())
     account = get_account()
-    # pass the price feed address to our fundme contract
-
-    # if we are on a persistent network like rinkeby, use the associated address
-    # otherwise, deploy mocks
-    if network.show_active() != "development":
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         price_feed_address = config["networks"][network.show_active()][
             "eth_usd_price_feed"
         ]
     else:
-        print(f"The active network is {network.show_active()}")
-        print("Deploying Mocks...")
-        mock_aggregator = MockV3Aggregator.deploy(
-            18, 200000000000000000000, {"from": account}
-        )
-        price_feed_address = mock_aggregator.address
-        print("Mocks Deployed")
+        deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1].address
 
     fund_me = FundMe.deploy(
         price_feed_address,
