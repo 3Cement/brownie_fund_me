ff55e6b42c335830543495d87ed88147e1a08016
daniel <danielmilewski123@gmail.com>
2023-09-19 16:04:53 +0200

    Create MockV3Aggregator and deploy different versions in case of development/goerli or different chain

--- contracts/FoundMe.sol
+++ contracts/FoundMe.sol
@@ -12,8 +12,10 @@ contract FundMe {
     mapping(address => uint256) public addressToAmountFunded;
     address[] public funders;
     address public owner;
+    AggregatorV3Interface public priceFeed;
 
-    constructor() public {
+    constructor(address _priceFeed) public {
+        priceFeed = AggregatorV3Interface(_priceFeed);
         owner = msg.sender;
     }
 
@@ -26,12 +28,10 @@ contract FundMe {
     }
 
     function getVersion() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         return priceFeed.version();
     }
 
     function getPrice() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         (,int answer,,,) = priceFeed.latestRoundData();
         return uint256(answer * 10000000000);
     }
